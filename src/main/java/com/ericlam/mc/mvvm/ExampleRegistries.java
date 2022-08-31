120ef1139410397ab000a4067bb3bcb520a9bf4d
sysnapse <eric1008818@gmail.com>
2022-08-31 09:31:37 +0800

    added command to registries

--- src/main/java/com/ericlam/mc/mvvm/ExampleRegistries.java
+++ src/main/java/com/ericlam/mc/mvvm/ExampleRegistries.java
@@ -4,12 +4,18 @@ import com.ericlam.mc.eld.bukkit.CommandNode;
 import com.ericlam.mc.eld.bukkit.ComponentsRegistry;
 import com.ericlam.mc.eld.registration.CommandRegistry;
 import com.ericlam.mc.eld.registration.ListenerRegistry;
+import com.ericlam.mc.mvvm.commands.ExamplesCommand;
+import com.ericlam.mc.mvvm.commands.MultiPlayerChatUI;
+import com.ericlam.mc.mvvm.commands.TicTaeToeUI;
 import org.bukkit.event.Listener;
 
 public final class ExampleRegistries implements ComponentsRegistry {
     @Override
     public void registerCommand(CommandRegistry<CommandNode> commandRegistry) {
-
+        commandRegistry.command(ExamplesCommand.class, c -> {
+            c.command(TicTaeToeUI.class);
+            c.command(MultiPlayerChatUI.class);
+        });
     }
 
     @Override
